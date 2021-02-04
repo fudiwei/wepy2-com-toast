cd2457a7e829932116dba6f4162beed23a9888e2
Fu Diwei <fudiwei@sina.com>
2021-02-05 00:17:36 +0800

    feat: rewrite TS declaration

--- com-toast.d.ts
+++ com-toast.d.ts
@@ -1,20 +1,22 @@
-export interface ComponentToast {
+export interface WepyComponentToast {
     /**
      * 显示消息对话框。
      * @param {String | Object} options 
      */
-    show(
-        options:
-            | string
-            | {
-                  content: string;
-                  position?: 'top' | 'center' | 'bottom';
-                  duration?: number;
-              }
-    ): void;
+    show(options: string | WepyComponentToast.ToastOptions): void;
     
     /**
      * 隐藏消息对话框。
      */
     hide(): void;
+}
+
+export namespace WepyComponentToast {
+    export type ToastPositions = 'top' | 'center' | 'bottom';
+
+    export interface ToastOptions {
+        content: string;
+        position?: ToastPositions;
+        duration?: number;
+    }
 }
